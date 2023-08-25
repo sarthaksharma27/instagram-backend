abcb8aee1295655affa723f2e421f852de1bca55
Sarthak sharma <130299888+sarthaksharma27@users.noreply.github.com>
2023-08-25 17:16:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/ig/:username', (req, res) => {
+    let {username} = req.params;
+    const instadata = require('./instagram.json');
+    const data = instadata[username];
+    res.render('home.ejs', { data });
+});
+
