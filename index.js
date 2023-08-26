61fda6db37a464d512a92444fd1b7fe3865a569a
Sarthak sharma <130299888+sarthaksharma27@users.noreply.github.com>
2023-08-26 11:21:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
 
+app.use(express.static('public');
 app.set('view engine', 'ejs');
 
 app.get('/ig/:username', (req, res) => {
